8ed8b5d68a285dab9370b5007a4d22e63c31fce9
waqas837 <73779175+waqas837@users.noreply.github.com>
2021-07-28 17:57:18 +0500

    make it better

--- src/components/CourseCategories/CourseCategory.js
+++ src/components/CourseCategories/CourseCategory.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useStyles2 } from "../../Styles/Main.Styles";
 import { Grid, Box, Typography, Divider, Container } from "@material-ui/core";
 import image from "../../images/download.jpg";
+import image2 from "../../images/img2.jpg";
+import image3 from "../../images/img3.jpg";
+import image4 from "../../images/img4.jpg";
+import image5 from "../../images/imge1.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 const CourseCategory = () => {
@@ -31,53 +35,39 @@ const CourseCategory = () => {
         />
       </Box>
       {/* caresoule */}
-     
-      <Box mb={5}>
-          <Carousel
-           navButtonsAlwaysVisible
-           autoPlay={false}
-           showThumbs={false}
-           showIndicators={false}
-           showStatus={0}
-           centerMode
-           centerSlidePercentage={30}
-           >
-  <div style={{padding:"10px"}}>
-    <img src={image} alt="" width="100px" height="200px"/>
-    
-    
-  </div>
-  
-  <div style={{padding:"10px"}}>
-    <img src={image} alt="" width="100px" height="200px"/>
-      
-    
-  </div>
-  <div style={{padding:"10px"}}>
-    <img src={image} alt="" width="100px" height="200px"/>
-      
-     
-    
-  </div>
-  <div style={{padding:"10px"}}>
-    <img src={image} alt="" width="100px" height="200px"/>
-     
-    
-  </div>
-  <div style={{padding:"10px"}}>
-    <img src={image} alt="" width="100px" height="200px"/>
-      
-    
-  </div>
-  <div style={{padding:"10px"}}>
-    <img src={image} alt="" width="100px" height="200px"/>
-      
-    
-  </div>
 
-           </Carousel>
-          </Box>
-    
+      <Box mb={5}>
+        <Carousel
+          navButtonsAlwaysVisible
+          autoPlay={false}
+          showThumbs={false}
+          showIndicators={false}
+          showStatus={0}
+          centerMode
+          centerSlidePercentage={30}
+          infiniteLoop={true}
+        >
+          <div style={{ padding: "10px" }}>
+            <img src={image} alt="" width="100px" height="200px"/>
+          </div>
+
+          <div style={{ padding: "10px" }}>
+            <img src={image2} alt="" width="100px" height="200px" />
+          </div>
+          <div style={{ padding: "10px" }}>
+            <img src={image3} alt="" width="100px" height="200px" />
+          </div>
+          <div style={{ padding: "10px" }}>
+            <img src={image4} alt="" width="100px" height="200px" />
+          </div>
+          <div style={{ padding: "10px" }}>
+            <img src={image5} alt="" width="100px" height="200px" />
+          </div>
+          <div style={{ padding: "10px" }}>
+            <img src={image3} alt="" width="100px" height="200px" />
+          </div>
+        </Carousel>
+      </Box>
     </div>
   );
 };
